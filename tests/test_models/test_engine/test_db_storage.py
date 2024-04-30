3b06d403711793b3d70562bb0444e6da9691ca0a
Chijindu Nwokeohuru <chiji4collage@gmail.com>
2024-04-30 04:50:31 +0100

    commit update test_db_storage.py

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -86,7 +86,7 @@ class TestFileStorage(unittest.TestCase):
         new_state = State(**state_data)
         models.storage.new(new_state)
         session = models.storage._DBStorage__session
-        retrieved_state = session.query(State).filter_by(id=new_state_id).first()# noqa
+        retrieved_state = session.query(State).filter_by(id=new_state_id).first()  # noqa
         self.assertEqual(retrieved_state.id, new_state.id)
         self.assertEqual(retrieved_state.name, new_state.name)
         self.assertIsNotNone(retrieved_state)
@@ -99,7 +99,7 @@ class TestFileStorage(unittest.TestCase):
         models.storage.new(new_state)
         models.storage.save()
         session = models.storage._DBStorage__session
-        retrieved_state = session.query(State).filter_by(id=new_state_id).first()# noqa
+        retrieved_state = session.query(State).filter_by(id=new_state_id).first()  # noqa
         self.assertEqual(retrieved_state.id, new_state.id)
         self.assertEqual(retrieved_state.name, new_state.name)
         self.assertIsNotNone(retrieved_state)
